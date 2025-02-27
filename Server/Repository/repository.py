d3a72ebed73cd1e457fc74616b9cb283cefc3bb2
Carlos Eduardo Fontes de Assis <rockadu@hotmail.com>
2025-02-27 18:12:30 -0300

    Repository server adjusts

--- Server/Repository/repository.py
+++ Server/Repository/repository.py
@@ -13,40 +13,44 @@ def get_db_connection():
 def setup_database():
     with get_db_connection() as db_connection:
         cursor = db_connection.cursor()
-        
-            # Criação da tabela Users
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS Users (
-            UserId INTEGER PRIMARY KEY AUTOINCREMENT,
-            Username TEXT NOT NULL UNIQUE,
-            PasswordHash TEXT NOT NULL,
-            Email TEXT NOT NULL UNIQUE,
-            CreatedAt TEXT DEFAULT CURRENT_TIMESTAMP,
-            IsActive INTEGER DEFAULT 1
-        )
-    """)
+    
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Users (
+                UserId INTEGER PRIMARY KEY AUTOINCREMENT,
+                Username TEXT NOT NULL UNIQUE,
+                PasswordHash TEXT NOT NULL,
+                Email TEXT NOT NULL UNIQUE,
+                CreatedAt TEXT DEFAULT CURRENT_TIMESTAMP,
+                IsActive INTEGER DEFAULT 1
+            )
+        """)
 
-    # Criação da tabela ActivityCount
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS ActivityCount (
-            Timestamp TEXT PRIMARY KEY,
-            LoggedUser TEXT NOT NULL,
-            MouseClicks INTEGER DEFAULT 0,
-            KeyPresses INTEGER DEFAULT 0,
-            MouseScroll INTEGER DEFAULT 0
-        )
-    """)
+        print("Criando tabela ActivityCount")
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS ActivityCount (
+                Timestamp TEXT PRIMARY KEY,
+                LoggedUser TEXT,
+                MouseClicks INTEGER DEFAULT 0,
+                KeyPresses INTEGER DEFAULT 0,
+                MouseScroll INTEGER DEFAULT 0,
+                Sync INTEGER DEFAULT 0
+            )
+        """)
+        print("Tabela ActivityCount criada")
 
-    # Criação da tabela WindowActivity
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS WindowActivity (
-            Id INTEGER PRIMARY KEY AUTOINCREMENT,
-            LoggedUser TEXT NOT NULL,
-            StartTime TEXT NOT NULL,
-            WindowTitle TEXT NOT NULL,
-            ProgramName TEXT NOT NULL
-        )
-    """)
+        print("Criando tabela WindowActivity")
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS WindowActivity (
+                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                LoggedUser TEXT NOT NULL,
+                StartTime TEXT NOT NULL,
+                WindowTitle TEXT NOT NULL,
+                ProgramName TEXT NOT NULL,
+                ActivityDuration INTEGER DEFAULT 0,
+                Sync INTEGER DEFAULT 0
+            )
+        """)
+        print("Tabela WindowActivity criada")
 
     # Confirma as alterações e fecha a conexão
     db_connection.commit()
